04cddb8779979312e3271b816ecd3092da46974c
Jordan Burnett <jordankburnett@gmail.com>
2015-06-06 16:58:16 -0400

    Added passport authentication to backend. still requires a way to handle this in Angular

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var jwt = require('express-jwt');
 
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
+var auth = jwt({secret:'SECRET', userProperty: 'payLoad'});
+ 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -28,9 +33,45 @@ router.get('/posts/:post', function(req,res,next){
     })
 });
 
+/* POST Register a new user*/
+router.post('/register', function(req, res, next){
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({message:'Please fill out all fields'});
+    };
+
+    var user = new User();
+
+    user.username = req.body.username;
+    user.setPassword(req.body.password);
+
+    user.save(function(err){
+        if (err) {return next(err);};
+
+        return res.json({token:user.generateJWT()});
+    });
+});
+
+/*POST login */
+router.post('/login', function(req, res, next){
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({message:'Please fill out all fields'});
+    };
+
+    passport.authenticate('local', function(err, user, info){
+        if (err) {return next(err);};
+
+        if (user) {
+            return res.json({token:user.generateJWT()});
+        } else {
+            return res.status(401).json(info);
+        };
+    })(req,res,next);
+});
+
 /* POST - create new post */
-router.post('/posts',function(req,res,next){
+router.post('/posts', auth, function(req,res,next){
     var post = new Post(req.body);
+    post.author = req.payLoad.username;
 
     post.save(function(err, post){
         if (err) {next(err);};
@@ -40,9 +81,10 @@ router.post('/posts',function(req,res,next){
 });
 
 /* POST - Create a comment to a post */
-router.post('/posts/:post/comments' , function(req,res,next){
+router.post('/posts/:post/comments', auth , function(req,res,next){
     var comment = new Comment(req.body);
     comment.post = req.post;
+    comment.author = req.payLoad.username;
 
     comment.save(function(err, comment){
         if (err) {return next(err);};
@@ -57,7 +99,7 @@ router.post('/posts/:post/comments' , function(req,res,next){
 });
 
 /*PUT - Upvote a post*/
-router.put('/posts/:post/upvote', function(req,res, next){
+router.put('/posts/:post/upvote', auth, function(req,res, next){
     req.post.upvote(function(err, post){
         if (err) {return next(err);};
         res.json(post);
@@ -65,7 +107,7 @@ router.put('/posts/:post/upvote', function(req,res, next){
 });
 
 /*PUT - Upvote a comment on a post */
-router.put('/posts/:post/comments/:comment/upvote', function(req,res,next){
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req,res,next){
     req.comment.upvote(function(err, comment){
         if (err) {return next(err);};
         res.json(comment);
